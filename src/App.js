f8e9b00609ef4b817e87a85d8dfd5c5ba4c1afdd
johnmphelan <johnmphelan@gmail.com>
2022-03-07 13:51:02 +0000

    Zoo days

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <Router>
       <div className="ant-layout">
-        <Nav>Nav 2 baby </Nav>
+        <Nav>Nav 2 baby Another baby bayb baby</Nav>
         <Content style={{ padding: "0 50px" }}>
           <Routes>
             <Route path="/" exact element={<Home />} />
