edcaae92e312aac6c5b4712b8bc51b8a98a91dee
johnmphelan <johnmphelan@gmail.com>
2022-03-07 17:48:21 +0000

    Another wan 3

--- src/Data/TopLevelLocations.js
+++ src/Data/TopLevelLocations.js
@@ -1,7 +1,7 @@
 export const TopLevelLocations = [
     {
       title: "Leixlip",
-      imagePath: process.env.PUBLIC_URL + "/Images/1_wonderful_barn.png",
+      imagePath: "/Images/1_wonderful_barn.PNG",
       pathTo: "/leixlip",
     },
     {
